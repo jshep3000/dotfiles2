f14d85b273778dec83ddd3d2dc3554debf4296d1
James Sheppard <jsheppard@gmail.com>
2019-02-25 14:52:37 -0800

    Initial Setup (#1)
    * Initial Setup
    
    Remove ammonite
    Customize setup script for what I want
    Configure vim from my vim repo
    Guard around iTerm install
    Add spacefish theme
    Moving dotfiles into regular files for visibility
    Add tmux configuration

--- setup.sh
+++ setup.sh
@@ -8,31 +8,35 @@ fi
 
 brew update
 brew upgrade || true
+brew tap caskroom/cask
 brew bundle install --file=- <<-EOF
+brew "tmux"
 brew "fish"
 brew "vim"
-brew "the_silver_searcher"
-brew "tree"
-brew "sbt"
-brew "scala"
 EOF
+
+brew cask list iterm2 || brew cask install iterm2
+
+brew tap caskroom/fonts
+brew cask list font-hack-nerd-font || brew cask install font-hack-nerd-font
 
 sudo dscl . -create /Users/$USER UserShell /usr/local/bin/fish
 
 mkdir -p ~/.config/fish
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-ln -s "$SCRIPTPATH"/.config/fish/config.fish ~/.config/fish/config.fish || true
+ln -s "$SCRIPTPATH"/config/fish/config.fish ~/.config/fish/config.fish || true
 mkdir -p ~/.config/fish/functions
-ln -s "$SCRIPTPATH"/.config/fish/functions/* ~/.config/fish/functions/ || true
-ln -s "$SCRIPTPATH"/.config/fish/fishfile ~/.config/fish/fishfile || true
-mkdir -p ~/.ammonite/
-ln -s "$SCRIPTPATH"/predef.sc ~/.ammonite/predef.sc || true
-ln -s "$SCRIPTPATH"/.vimrc ~/.vimrc || true
+ln -s "$SCRIPTPATH"/config/fish/functions/* ~/.config/fish/functions/ || true
+ln -s "$SCRIPTPATH"/config/fish/fishfile ~/.config/fish/fishfile || true
+ln -s "$SCRIPTPATH"/tmux.conf ~/.tmux.conf || true
 
 VUNDLE_DIR="$HOME/.vim/bundle/Vundle.vim"
 if [ ! -d "$VUNDLE_DIR" ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git $VUNDLE_DIR
 fi
 
-vim +PluginInstall +qall
+bash < <(curl -Ls https://github.com/jshep3000/vim-jsheppard/raw/master/scripts/setup.sh)
+
+# True color tmux configuration
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
---
+++ vimrc
@@ -0,0 +1,32 @@
+set nocompatible
+filetype off
+
+set rtp+=~/.vim/bundle/Vundle.vim
+call vundle#begin()
+
+Plugin 'VundleVim/Vundle.vim'
+
+"Plugin 'gmarik/vundle'
+"Plugin 'altercation/vim-colors-solarized'
+"Plugin 'kien/ctrlp.vim'
+"Plugin 'tpope/vim-fugitive'
+"
+"Plugin 'closetag.vim'
+"Plugin 'sjl/gundo.vim'
+"
+"Plugin 'powerline/powerline'
+"Plugin 'scrooloose/nerdtree'
+"Plugin 'sukima/xmledit'
+"Plugin 'leshill/vim-json'
+"Plugin 'Townk/vim-autoclose'
+"Plugin 'mileszs/ack.vim'
+"Plugin 'vim-scripts/copy-as-rtf'
+"Plugin 'vim-scripts/camelcasemotion'
+"Plugin 'kien/rainbow_parentheses.vim'
+"Plugin 'Shougo/neocomplcache'
+
+Plugin 'jshep3000/vim-jsheppard'
+
+call vundle#end()
+filetype plugin indent on
+
